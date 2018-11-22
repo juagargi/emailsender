54aae1a65de58469f422bd031f60c66bb970ee12
Juan A. Garcia Pardo <juagargi@gmail.com>
2018-11-22 18:29:01 +0100

    Support command line switches

--- emailsender.go
+++ emailsender.go
@@ -3,13 +3,16 @@ package main
 import (
 	"bufio"
 	"errors"
+	"flag"
 	"fmt"
 	"io/ioutil"
 	"os"
 	"strings"
+	"time"
 
 	"github.com/keighl/postmark"
 	"github.com/sec51/goconf"
+	"golang.org/x/crypto/ssh/terminal"
 )
 
 const chunkSize = 49
@@ -96,22 +99,41 @@ func loadEmail() Email {
 	}
 }
 
-func askForConfirmation(thisMeansYes string) bool {
-	reader := bufio.NewReader(os.Stdin)
-	text, _ := reader.ReadString('\n')
-	text = text[:len(text)-1]
-	return text == thisMeansYes
+func askForConfirmation(thisMeansYes string, interactive bool) bool {
+	if interactive {
+		reader := bufio.NewReader(os.Stdin)
+		text, _ := reader.ReadString('\n')
+		text = text[:len(text)-1]
+		return text == thisMeansYes
+	} else {
+		// if there is output terminal, use it:
+		// fmt.Println("is terminal?:", terminal.IsTerminal(int(os.Stdout.Fd())), "answered")
+		if terminal.IsTerminal(int(os.Stdout.Fd())) {
+			for i := 10; i > 0; i-- {
+				if i%2 == 0 {
+					fmt.Printf("\nPress Ctrl-C to Cancel (%d seconds remaining) ...", i)
+				}
+				time.Sleep(1 * time.Second)
+			}
+			fmt.Println("")
+		} else {
+			fmt.Println("y (auto answered, output is not terminal)")
+		}
+		return true
+	}
 }
 
 // Send connects to the PostMark email API and sends the email
-func Send(conf *Config, mail *Email) error {
-	// ask for confirmation
+func Send(conf *Config, mail *Email, interactive bool) error {
 	fmt.Println("---------------------------")
 	fmt.Printf("From: %s\nSubject: %s\nBody:\n%s\n", mail.From, mail.Subject, mail.Body)
 	fmt.Println("---------------------------")
-	fmt.Print("Continue? (y/n) ")
-	if !askForConfirmation("y") {
-		return errors.New("Cancelled by user")
+	if interactive {
+		// ask for confirmation
+		fmt.Print("Continue? (y/n) ")
+		if !askForConfirmation("y", interactive) {
+			return errors.New("Cancelled by user")
+		}
 	}
 	recipientBuckets := make([][]string, (len(mail.To)-1)/chunkSize+1)
 	row := 0
@@ -130,10 +152,9 @@ func Send(conf *Config, mail *Email) error {
 	fmt.Printf("To: %s\n", strings.Join(allRecipients, "\n"))
 	fmt.Println("---------------------------")
 	fmt.Print("Continue? (y/n) ")
-	if !askForConfirmation("y") {
+	if !askForConfirmation("y", interactive) {
 		return errors.New("Cancelled by user")
 	}
-
 	for bucketIndex, bucket := range recipientBuckets {
 		fmt.Printf("Sending chunk %d / %d ... %s\n", bucketIndex+1, len(recipientBuckets), strings.Join(bucket, ","))
 		client := postmark.NewClient(conf.pm_server_token, conf.pm_account_token)
@@ -157,9 +178,37 @@ func Send(conf *Config, mail *Email) error {
 }
 
 func main() {
+	var email Email
+	toFlag := flag.String("to", "", "Recipients email addresses separated with ;")
+	subjectFlag := flag.String("subject", "", "Subject")
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, "Usage of %s (if one email parameter specified, email body will be read from Stdin):\n", os.Args[0])
+		flag.PrintDefaults()
+	}
+	flag.Parse()
 	conf := loadConf()
-	email := loadEmail()
-	err := Send(&conf, &email)
+	interactive := false
+	if *toFlag != "" || *subjectFlag != "" {
+		reader := bufio.NewReader(os.Stdin)
+		b, err := ioutil.ReadAll(reader)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error reading Stdin: %v\n", err)
+			os.Exit(1)
+		}
+		email.Body = string(b)
+		if *toFlag == "" || *subjectFlag == "" {
+			fmt.Fprintln(os.Stderr, "If setting one field, all fields must be set!")
+			flag.Usage()
+			os.Exit(1)
+		}
+		email.From = fromAddress
+		email.To = strings.Split(*toFlag, ";")
+		email.Subject = *subjectFlag
+	} else {
+		email = loadEmail()
+		interactive = true
+	}
+	err := Send(&conf, &email, interactive)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
